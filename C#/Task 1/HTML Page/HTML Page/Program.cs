6a105788e9a14dad5aee80ff95d75a49a4e30caf
Ahmed Turky <Ahmedturky75@gmail.com>
2023-05-30 00:31:40 +0300

    4 Tasks

---
+++ C#/Task 1/HTML Page/HTML Page/Program.cs
@@ -0,0 +1,140 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+using System.Text;
+
+namespace HTML_Page
+{
+
+
+
+
+    class Employee
+    {
+        public string Id { get; set; }
+        public string EmployeeName { get; set; }
+        public string StarTimeUtc { get; set; }
+        public string EndTimeUtc { get; set; }
+        public string EntryNotes { get; set; }
+        public string Name { get; set; }
+        public int TotalTimeWorked { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                // Download the Bootstrap CSS file
+                string bootstrapCssUrl = "https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css";
+                string bootstrapCssFilePath = "bootstrap.min.css";
+                DownloadFile(bootstrapCssUrl, bootstrapCssFilePath);
+
+                // Make the GET request to the API endpoint
+                string apiUrl = "https://rc-vault-fap-live-1.azurewebsites.net/api/gettimeentries?code=vO17RnE8vuzXzPJo5eaLLjXjmRW07law99QTD90zat9FfOQJKKUcgQ==";
+                string json = MakeGetRequest(apiUrl);
+
+                // Deserialize the JSON response into a list of Employee objects
+                List<Employee> employees = JsonConvert.DeserializeObject<List<Employee>>(json);
+
+                // Group employees by name and calculate total time worked for each employee
+                var groupedEmployees = employees.GroupBy(emp => emp.EmployeeName)
+                                                .Select(group => new Employee
+                                                {
+                                                    EmployeeName = group.Key,
+                                                    TotalTimeWorked = group.Sum(emp => CalculateTimeDifference(emp))
+                                                })
+                                                .ToList();
+
+                // Sort the employees by total time worked in descending order
+                groupedEmployees.Sort((emp1, emp2) => emp2.TotalTimeWorked.CompareTo(emp1.TotalTimeWorked));
+
+                // Generate the HTML table with Bootstrap classes
+                string tableHtml = GenerateHtmlTable(groupedEmployees);
+
+                // Create an HTML file and write the table data
+                string filePath = "employee_table.html";
+                File.WriteAllText(filePath, tableHtml);
+
+                // Add Bootstrap CSS file link to the HTML file
+                AddBootstrapCssLink(filePath, bootstrapCssFilePath);
+
+                Console.WriteLine("HTML file created: " + filePath);
+            }
+
+            static void DownloadFile(string url, string filePath)
+            {
+                using (WebClient client = new WebClient())
+                {
+                    client.DownloadFile(url, filePath);
+                }
+            }
+
+            static string MakeGetRequest(string url)
+            {
+                using (WebClient client = new WebClient())
+                {
+                    return client.DownloadString(url);
+                }
+            }
+
+            static int CalculateTimeDifference(Employee employee)
+            {
+                DateTime startTime = DateTime.Parse(employee.StarTimeUtc);
+                DateTime endTime = DateTime.Parse(employee.EndTimeUtc);
+                TimeSpan timeDifference = endTime - startTime;
+                return (int)timeDifference.TotalHours;
+            }
+
+            static string GenerateHtmlTable(List<Employee> employees)
+            {
+                StringBuilder sb = new StringBuilder();
+
+                // Open the table tag and add Bootstrap classes
+                sb.AppendLine("<table class=\"table table-bordered table-striped\">");
+
+                // Create table header with Bootstrap classes
+                sb.AppendLine("<tr class=\"thead-dark\"><th>Name</th><th>Total Time Worked</th></tr>");
+
+                // Create table rows with Bootstrap classes
+                foreach (Employee employee in employees)
+                {
+                    string rowColor = employee.TotalTimeWorked < 100 ? "table-danger" : "";
+                    sb.AppendLine($"<tr class=\"{rowColor}\"><td>{employee.EmployeeName}</td><td>{employee.TotalTimeWorked}</td></tr>");
+                }
+
+                // Close the table tag
+                sb.AppendLine("</table>");
+
+                return sb.ToString();
+            }
+
+            static void AddBootstrapCssLink(string htmlFilePath, string cssFilePath)
+            {
+                string htmlContent = File.ReadAllText(htmlFilePath);
+
+                string cssLinkTag = $"<link rel=\"stylesheet\" href=\"{cssFilePath}\">";
+                int headIndex = htmlContent.IndexOf("<head>");
+
+                if (headIndex != -1)
+                {
+                    int insertIndex = headIndex + "<head>".Length;
+                    htmlContent = htmlContent.Insert(insertIndex, cssLinkTag);
+                }
+                else
+                {
+                    int bodyIndex = htmlContent.IndexOf("<body>");
+                    if (bodyIndex != -1)
+                    {
+                        htmlContent = htmlContent.Insert(bodyIndex, $"<head>{cssLinkTag}</head>");
+                    }
+                    else
+                    {
+                        htmlContent = $"<head>{cssLinkTag}</head>{htmlContent}";
+                    }
+                }
+
+                File.WriteAllText(htmlFilePath, htmlContent);
+            }
+        }
+    }
+}
---
+++ C#/Task 2/Pie Chart/Pie Chart/Program.cs
@@ -0,0 +1,154 @@
+﻿using System.Drawing;
+using System.Text.Json;
+namespace Pie_Chart
+{
+    internal class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            try
+            {
+                // Fetch JSON data from the REST endpoint
+                string json = await FetchJsonData();
+
+                // Parse the JSON data
+                var employees = ParseJsonData(json);
+
+
+                // Generate the pie chart
+                GeneratePieChart(getUniqueEmployees(employees));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+
+        static async System.Threading.Tasks.Task<string> FetchJsonData()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync("https://rc-vault-fap-live-1.azurewebsites.net/api/gettimeentries?code=vO17RnE8vuzXzPJo5eaLLjXjmRW07law99QTD90zat9FfOQJKKUcgQ==");
+                response.EnsureSuccessStatusCode();
+                string json = await response.Content.ReadAsStringAsync();
+                return json;
+            }
+        }
+
+        static List<Employee> ParseJsonData(string json)
+        {
+            var employees = new List<Employee>();
+            JsonDocument document = JsonDocument.Parse(json);
+
+            // Assuming the JSON data is an array of employee objects
+            foreach (JsonElement element in document.RootElement.EnumerateArray())
+            {
+                string name = element.GetProperty("EmployeeName").GetString();
+                string id = element.GetProperty("Id").GetString();
+                string startTimeUtcString = element.GetProperty("StarTimeUtc").GetString();
+                string endTimeUtcString = element.GetProperty("EndTimeUtc").GetString();
+
+                // Parse the DateTime values
+                DateTime startTimeUtc = DateTime.Parse(startTimeUtcString);
+                DateTime endTimeUtc = DateTime.Parse(endTimeUtcString);
+
+                employees.Add(new Employee(name, id, startTimeUtc, endTimeUtc));
+
+            }
+
+            return employees;
+        }
+
+        static void GeneratePieChart(Dictionary<string, int> employees)
+        {
+            using (Bitmap image = new Bitmap(800, 600))
+            {
+                using (Graphics graphics = Graphics.FromImage(image))
+                {
+                    // Clear the image with a white background
+                    graphics.Clear(Color.White);
+
+                    // Calculate the total time worked by all employees
+                    int totalWorkedTime = 0;
+                    foreach (var employee in employees)
+                    {
+                        totalWorkedTime += employee.Value;
+
+                    }
+
+                    // Draw the pie chart
+                    int startAngle = 0;
+                    foreach (var employee in employees)
+                    {
+                        float percentage = (float)employee.Value / totalWorkedTime;
+
+                        int sweepAngle = (int)(percentage * 360);
+
+                        // Set a random color for each employee
+                        Color color = Color.FromArgb(new Random().Next(256), new Random().Next(256), new Random().Next(256));
+
+                        // Draw the pie slice
+                        graphics.FillPie(new SolidBrush(color), new Rectangle(200, 100, 400, 400), startAngle, sweepAngle);
+
+                        // Update the start angle for the next slice
+                        startAngle += sweepAngle;
+                    }
+
+                    // Save the image as a PNG file
+                    image.Save("pie_chart.png");
+                }
+            }
+
+            Console.WriteLine("Pie chart generated successfully!");
+        }
+
+        static public Dictionary<string, int> getUniqueEmployees(List<Employee> employees)
+        {
+            Dictionary<string, int> totalTimeWorked = new Dictionary<string, int>();
+            foreach (var employee in employees)
+            {
+                if (totalTimeWorked.ContainsKey(employee.Name ?? "No name"))
+                {
+                    // Employee already exists in the dictionary, update the total time worked
+                    totalTimeWorked[employee.Name ?? "No name"] += employee.getDurationPerDay();
+                }
+                else
+                {
+                    // New employee, add to the dictionary
+                    totalTimeWorked.Add(employee.Name ?? "No name", employee.getDurationPerDay());
+                }
+            }
+            return totalTimeWorked;
+        }
+    }
+
+
+
+
+    class Employee
+    {
+        public string Id { get; set; }
+        public string EmployeeName { get; set; }
+        public DateTime StarTimeUtc { get; set; }
+        public DateTime EndTimeUtc { get; set; }
+        public string EntryNotes { get; set; }
+        public string Name { get; set; }
+        public int TotalTimeWorked { get; set; }
+
+        public Employee(string name, string id, DateTime starTimeUtc, DateTime endTimeUtc)
+        {
+            Name = name;
+            Id = id;
+            StarTimeUtc = starTimeUtc;
+            EndTimeUtc = endTimeUtc;
+        }
+        public int getDurationPerDay()
+        {
+            TimeSpan duration = EndTimeUtc - StarTimeUtc;
+            return (int)duration.TotalHours;
+        }
+    }
+
+
+}
+
